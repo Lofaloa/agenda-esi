483d357fdc73c6a6acd28a5a14caf281c8ad293f
Logan Farci <47923@etu.he2b.be>
2020-01-18 18:48:20 +0100

    Web service: provide a json response containing all events

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*-
+import json
+
 from odoo import http
+from odoo.http import request, Response
 
-# class AgendaEsi(http.Controller):
-#     @http.route('/agenda_esi/agenda_esi/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+import logging
+import datetime
 
-#     @http.route('/agenda_esi/agenda_esi/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('agenda_esi.listing', {
-#             'root': '/agenda_esi/agenda_esi',
-#             'objects': http.request.env['agenda_esi.agenda_esi'].search([]),
-#         })
+_logger = logging.getLogger(__name__)
 
-#     @http.route('/agenda_esi/agenda_esi/objects/<model("agenda_esi.agenda_esi"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('agenda_esi.object', {
-#             'object': obj
-#         })
+class AgendaEsi(http.Controller):
+
+    def event_to_dictionnary(self, event):
+        return {
+            "title": event.title,
+            "start": event.start_datetime,
+            "end": event.end_datetime
+        }
+
+
+    @http.route('/api/events/<string:name>', auth='public')
+    def index(self, **kw):
+        # TODO: search the event in which the current user is an attendee and
+        # having starting in the future
+        result = http.request.env['agenda_esi.event'].search([])
+        events = []
+        for e in result:
+            events.append(self.event_to_dictionnary(e))
+        headers = {'Content-Type': 'application/json'}
+        body = { 'results': { 'code': 200, 'events': events} }
+        return Response(json.dumps(body), headers=headers)
