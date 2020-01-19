6f416df5a38a80865c925f21533f72355360fd10
49737 <49737@etu.he2b.be>
2020-01-19 23:11:45 +0100

    Api : domains

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,9 +5,10 @@ from odoo import http
 from odoo.http import request, Response
 
 import logging
-import datetime
+from datetime import datetime
 
 _logger = logging.getLogger(__name__)
+
 
 class AgendaEsi(http.Controller):
 
@@ -18,15 +19,21 @@ class AgendaEsi(http.Controller):
             "end": event.end_datetime
         }
 
-
-    @http.route('/api/events/<string:name>', auth='public')
+    @http.route('/api/events/<string:login>', auth='public')
     def index(self, **kw):
         # TODO: search the event in which the current user is an attendee and
         # having starting in the future
-        result = http.request.env['agenda_esi.event'].search([])
+        current_date = str(datetime.now())
+        values = dict(kw)
+        user = http.request.env['res.users'].search(
+            [('login', '=', values['login']), ])
+
+        result = http.request.env['agenda_esi.event'].search(
+            ['&', ('start_datetime', '>=', current_date),
+                (user.partner_id, 'in', 'attendees.ids'), ])
         events = []
         for e in result:
             events.append(self.event_to_dictionnary(e))
         headers = {'Content-Type': 'application/json'}
-        body = { 'results': { 'code': 200, 'events': events} }
+        body = {'results': {'code': 200, 'events': events}}
         return Response(json.dumps(body), headers=headers)
